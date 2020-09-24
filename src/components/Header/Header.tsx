dd8ed92cbeb75f924bd12aefdd83e0cae29aaa1e
Tim Seryakov <t.seryakov@gmail.com>
2020-09-24 18:52:20 +0300

    SWISH SWISH logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,10 @@ import React from 'react';
 export const Header = () => {
 
   return (
-      <header className="p-5 bg-blue-300 h-20">
-        <h1 className="font-bold text-2xl font-play">Swish Swish Social Network</h1>
+      <header className="p-5 bg-gray-900 h-20">
+        <h1 className="font-bold text-4xl font-play uppercase italic text-white font-bold relative">Swish Swish
+          <span className="uppercase bg-gray-800 text-gray-400 ml-4 px-2 py-1 rounded-sm text-sm inline-block absolute -translate-y-1/2">beta</span></h1>
+
       </header>
   )
 }
