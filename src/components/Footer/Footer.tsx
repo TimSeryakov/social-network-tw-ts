f1795b22697d05bac0c4bff4cdfdcc9fa75ae592
Tim Seryakov <t.seryakov@gmail.com>
2020-09-25 10:52:23 +0300

    some fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,10 @@ import React, {FC} from 'react';
 export const Footer: FC = () => (
     <footer className="h-12 bg-theme-bg-primary text-center border-t border-theme-border">
       <p className="text-theme-text p-3">
-        Swish Swish Training Project © 2020 🐱‍👤
+        Swish Swish Training Project © 2020 <span role="img" aria-label="samurai-cat">🐱‍👤</span>
         <a
             className="hover:text-white"
-            href="https://it-incubator.by" target="_blank">it-incubator.by
+            href="https://it-incubator.by" target="_blank" rel="noopener noreferrer">it-incubator.by
         </a>
       </p>
     </footer>
