7e217cc43cf27fa2c4732904fc54ca42d4c2edb5
Tim Seryakov <t.seryakov@gmail.com>
2020-09-25 19:09:34 +0300

    Messages styles done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -18,8 +18,12 @@ const DialogItem = (props: DialogItemType) => {
 
   return (
       <div className="flex">
-        <NavLink className="flex block border-b border-theme-border items-center hover:text-white text-theme-text pl-3 flex-grow" to={path + props.id}>
-          <img className="inline-block w-12 h-12 bg-theme-bg-secondary rounded-full border border-theme-border flex items-center justify-center my-1 mr-3" src={props.avatar} alt=""/>
+        <NavLink
+            className="flex block border-b border-theme-border items-center hover:text-white text-theme-text pl-3 flex-grow"
+            to={path + props.id}>
+          <img
+              className="inline-block w-12 h-12 bg-theme-bg-secondary rounded-full border border-theme-border flex items-center justify-center my-1 mr-3"
+              src={props.avatar} alt=""/>
           {props.name}
         </NavLink>
       </div>
@@ -28,43 +32,51 @@ const DialogItem = (props: DialogItemType) => {
 
 type MessagePropsType = {
   text: string
+  incoming: boolean
 }
 
 const Message = (props: MessagePropsType) => {
-  return (
-      <div className="bg-theme-bg-secondary m-3 p-3 rounded-md text-theme-text">{props.text}</div>
-  )
+  const incomingStyle = "bg-theme-bg-secondary rounded-md text-theme-text p-3 my-3 mr-10 ml-3"
+  const outGoing = "bg-theme-bg-third rounded-md text-theme-text p-3 my-3 ml-12 mr-3"
+
+  if (props.incoming) {
+    return (<div className={incomingStyle}>{props.text}</div>)
+  } else {
+    return (<div className={outGoing}>{props.text}</div>)
+  }
 }
 
-export const Dialogs = () => {
-  return (
-      <section className="h-full flex flex-col">
+  export const Dialogs = () => {
+    return (
+        <section className="h-full flex flex-col">
 
-       <PageTitle title="Your dialogs"/>
+          <PageTitle title="Your dialogs"/>
 
-        <div className="flex flex-auto flex-grow">
+          <div className="flex flex-auto flex-grow">
 
-          <div className="bg-theme-bg-primary border-r border-theme-border w-4/12">
-            <div className="">
-              <h3 className="py-2 px-4 border-t border-b border-theme-border text-theme-text bg-theme-bg-secondary">Users</h3>
+            <div className="bg-theme-bg-primary border-r border-theme-border w-4/12">
+              <div className="">
+                <h3 className="py-2 px-4 border-t border-b border-theme-border text-theme-text bg-theme-bg-secondary">Friends</h3>
+              </div>
+              <DialogItem name="Maxy" id={1} avatar={samurai}/>
+              <DialogItem name="Olya" id={2} avatar={fuji}/>
+              <DialogItem name="Vika" id={3} avatar={lionstatue}/>
+              <DialogItem name="Masha" id={4} avatar={luckycat}/>
             </div>
-            <DialogItem name="Maxy" id={1} avatar={samurai}/>
-            <DialogItem name="Olya" id={2} avatar={fuji}/>
-            <DialogItem name="Vika" id={3} avatar={lionstatue}/>
-            <DialogItem name="Masha" id={4} avatar={luckycat}/>
+
+            <div className="bg-theme-bg-primary border-t border-theme-border w-8/12 flex-auto">
+              <Message incoming={true} text="Hi"/>
+              <Message incoming={true} text="How r you"/>
+              <Message incoming={true} text="i want to talk"/>
+              <Message incoming={true} text="answer me"/>
+              <Message incoming={true} text="please"/>
+              <Message incoming={false} text="Hi, im there!"/>
+              <Message incoming={true} text="really 👻"/>
+            </div>
           </div>
 
-          <div className="bg-theme-bg-primary border-t border-theme-border w-full flex-auto">
-            <Message text="Hi"/>
-            <Message text="How r you"/>
-            <Message text="i want to talk"/>
-            <Message text="answer me"/>
-            <Message text="please"/>
-          </div>
-        </div>
 
-
-      </section>
-  )
-}
+        </section>
+    )
+  }
 
