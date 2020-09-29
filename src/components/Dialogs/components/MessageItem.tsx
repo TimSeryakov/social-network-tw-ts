f88e77f97c0b11cf327291b82c6d28f4fa246328
Tim Seryakov <t.seryakov@gmail.com>
2020-09-29 13:22:38 +0300

    message bubbles design edits

--- src/components/Dialogs/components/MessageItem.tsx
+++ src/components/Dialogs/components/MessageItem.tsx
@@ -6,8 +6,8 @@ type MessageItemPropsType = {
 }
 
 export const MessageItem = (props: MessageItemPropsType) => {
-  const incomingStyle = "bg-theme-bg-secondary rounded-md text-theme-text p-3 my-3 mr-10 ml-3"
-  const outGoingStyle = "bg-theme-bg-third rounded-md text-theme-text p-3 my-3 ml-12 mr-3"
+  const incomingStyle = "bg-theme-bg-secondary rounded-md rounded-bl-none text-theme-text p-3 my-3 mr-10 ml-3"
+  const outGoingStyle = "bg-theme-bg-third rounded-md rounded-br-none text-theme-text p-3 my-3 ml-12 mr-3"
 
   return (<div className={props.incoming ? incomingStyle : outGoingStyle}>{props.text}</div>)
 
