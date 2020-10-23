de98b8f5df28a7499406f0ad3f3a12914d9f161f
Tim Seryakov <t.seryakov@gmail.com>
2020-10-23 19:56:24 +0300

    MainMenu responsive

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -9,46 +9,45 @@ type SidebarPropsType ={
 
 export const MainMenu = (props: SidebarPropsType) => {
 
-  const MainMenuStyle = `${parseBordersProps(props.borders)} w-56 bg-theme-bg-third flex-shrink-0`
+  const MainMenuStyle = `${parseBordersProps(props.borders)} sm:relative w-full fixed bottom-0 sm:w-56 bg-theme-bg-third flex-shrink-0`
 
 
   return (
       <aside className={MainMenuStyle}>
-        <ul className="">
+        <ul className="flex flex-row justify-around sm:block sm:border-0 border-t border-theme-border">
           <li className="border-b border-theme-border">
             <NavLink to={"/profile"}
-                     className="text-xl text-theme-text block border-l-2 border-theme-bg-third hover:text-white py-4 pl-6"
-                     activeClassName="bg-theme-bg-primary border-l-2 border-theme-accent text-theme-white">
-              <i className="far fa-user mr-3"/> Profile
+                     className="block px-6 py-4 text-xl border-b-2 sm:pl-6 sm:border-b-0 sm:border-l-2 text-theme-text border-theme-bg-third hover:text-white"
+                     activeClassName="bg-theme-bg-primary border-b-2 sm:border-b-0 sm:border-l-2 border-theme-accent text-theme-white">
+              <i className="sm:mr-4 far fa-user"/> <span className="hidden sm:inline">Profile</span>
             </NavLink>
           </li>
           <li className="border-b border-theme-border">
             <NavLink to={"/dialogs"}
-                     className="text-xl text-theme-text block border-l-2 border-theme-bg-third hover:text-white py-4 pl-6"
-                     activeClassName="bg-theme-bg-primary border-l-2 border-theme-accent text-theme-white">
-
-              <i className="far fa-envelope mr-3"/> Messenger
+                     className="block px-6 py-4 text-xl border-b-2 sm:pl-6 sm:border-b-0 sm:border-l-2 text-theme-text border-theme-bg-third hover:text-white"
+                     activeClassName="bg-theme-bg-primary border-b-2 sm:border-b-0 sm:border-l-2 border-theme-accent text-theme-white">
+              <i className="sm:mr-4 far fa-envelope"/> <span className="hidden sm:inline">Messenger</span>
             </NavLink>
           </li>
           <li className="border-b border-theme-border">
             <NavLink to={"/news"}
-                     className="text-xl text-theme-text block border-l-2 border-theme-bg-third hover:text-white py-4 pl-6"
-                     activeClassName="bg-theme-bg-primary border-l-2 border-theme-accent text-theme-white">
-              <i className="fas fa-asterisk mr-3"/> News
+                     className="block px-6 py-4 text-xl border-b-2 sm:pl-6 sm:border-b-0 sm:border-l-2 text-theme-text border-theme-bg-third hover:text-white"
+                     activeClassName="bg-theme-bg-primary border-b-2 sm:border-b-0 sm:border-l-2 border-theme-accent text-theme-white">
+              <i className="sm:mr-4 fas fa-asterisk"/> <span className="hidden sm:inline">News</span>
             </NavLink>
           </li>
           <li className="border-b border-theme-border">
             <NavLink to={"/music"}
-                     className="text-xl text-theme-text block border-l-2 border-theme-bg-third hover:text-white py-4 pl-6"
-                     activeClassName="bg-theme-bg-primary border-l-2 border-theme-accent text-theme-white">
-              <i className="fas fa-headphones-alt mr-3"/> Music
+                     className="block px-6 py-4 text-xl border-b-2 sm:pl-6 sm:border-b-0 sm:border-l-2 text-theme-text border-theme-bg-third hover:text-white"
+                     activeClassName="bg-theme-bg-primary border-b-2 sm:border-b-0 sm:border-l-2 border-theme-accent text-theme-white">
+              <i className="sm:mr-3 fas fa-headphones-alt"/> <span className="hidden sm:inline">Music</span>
             </NavLink>
           </li>
           <li className="border-b border-theme-border">
             <NavLink to={"/settings"}
-                     className="text-xl text-theme-text block border-l-2 border-theme-bg-third hover:text-white py-4 pl-6"
-                     activeClassName="bg-theme-bg-primary border-l-2 border-theme-accent text-theme-white">
-              <i className="fas fa-cog mr-3"/> Settings
+                     className="block px-6 py-4 text-xl border-b-2 sm:pl-6 sm:border-b-0 sm:border-l-2 text-theme-text border-theme-bg-third hover:text-white"
+                     activeClassName="bg-theme-bg-primary border-b-2 sm:border-b-0 sm:border-l-2 border-theme-accent text-theme-white">
+              <i className="sm:mr-3 fas fa-cog"/> <span className="hidden sm:inline">Settings</span>
             </NavLink>
           </li>
         </ul>
