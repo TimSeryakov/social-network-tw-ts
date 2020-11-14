a07be76c3e892383a3aa8d1a7d8b2d05f0879a10
Tim Seryakov <t.seryakov@gmail.com>
2020-11-14 16:27:20 +0300

    lesson 54 (Have a look the video of Lesson 54 again)
    Took 20 minutes

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,13 +13,10 @@ type UsersPropsType = {
 
 export class Users extends React.Component<UsersPropsType> {
 
-  constructor(props: any) {
-    super(props)
-
+  componentDidMount() {
     axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
       this.props.setUsersFn(response.data.items)
     })
-
   }
 
 
